fa1fbc6bd8cdf5a9cda78ffe9c82a07c96b9249d
mindnervestech <jagbir.paul@gmail.com>
2015-01-11 21:21:03 +0530

    email add to Bcc

--- app/code/core/Mage/Sales/Model/Order.php
+++ app/code/core/Mage/Sales/Model/Order.php
@@ -1566,10 +1566,13 @@ class Mage_Sales_Model_Order extends Mage_Sales_Model_Abstract
 		//email setting ..........................
 		$mailer = Mage::getModel('core/email_template_mailer');
         $emailInfo = Mage::getModel('core/email_info');
-	    $emailInfo->addTo("nicolas.gaultier1983@gmail.com", "Chouettes Lunettes");
-  //     $emailInfo->addTo("muneshwarpankaj@gmail.com", "Chouettes Lunettes");
-      //  $emailInfo->addTo("muneshwarpankaj@gmail.com", "Chouetteslunettes");
-	//	$emailInfo->addBcc("muneshwarpankaj@gmail.com");
+	$emailInfo->addTo("nicolas.gaultier1983@gmail.com", "Chouettes Lunettes");
+ 
+	$emailInfo->addBcc("admin@chouetteslunettes.com");
+	$emailInfo->addBcc("alice.oesinger@ymail.com");
+	$emailInfo->addBcc("lionel.charpentier@gmail.com");
+//	$emailInfo->addBcc("admin@chouetteslunettes.com");
+	
         if ($copyTo && $copyMethod == 'bcc') {
             // Add bcc to customer email
             foreach ($copyTo as $email) {
